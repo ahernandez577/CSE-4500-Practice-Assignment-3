f776c650387dede162dc1fde2189779b473803db
Alex Hernandez <55168529+ahernandez577@users.noreply.github.com>
2022-03-03 12:45:26 -0800

    Create todos.blade.php

---
+++ resources/views/todos.blade.php
@@ -0,0 +1,19 @@
+@extends('adminlte::page')
+
+@section('title', 'To Dos')
+
+@section('content_header')
+    <h1>To Dos</h1>
+@stop
+
+@section('content')
+    <p>Here are the list of to dos</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
