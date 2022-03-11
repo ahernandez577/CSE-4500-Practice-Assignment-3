064a74a7b83473d9b9af076c0048401789c539cd
Alex Hernandez <55168529+ahernandez577@users.noreply.github.com>
2022-03-11 15:07:15 -0800

    Update 2022_03_11_223818_create_calendar_table.php

--- database/migrations/2022_03_11_223818_create_calendar_table.php
+++ database/migrations/2022_03_11_223818_create_calendar_table.php
@@ -16,6 +16,9 @@ return new class extends Migration
         Schema::create('calendar', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('title');
+            $table->DateTime('startTime');
+            $table->DateTime('endTime');
         });
     }
 
