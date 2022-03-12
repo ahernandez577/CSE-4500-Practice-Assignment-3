45748c1a84cc4e355aab7ec50f4d94258d6f2cf4
Alex Hernandez <55168529+ahernandez577@users.noreply.github.com>
2022-03-11 17:33:50 -0800

    Update Calendar.php

--- app/Models/Calendar.php
+++ app/Models/Calendar.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Calendar extends Model
 {
-    protected $fillable = ['title','progress'];
+    protected $fillable = ['title','events'];
     use HasFactory;
 }
